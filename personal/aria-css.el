ae18ba4c4e1c0a4d4c7d7cf48bc6dada02bd969b
siemonday <siemonday@gmail.com>
2015-01-21 22:13:36 +0800

    update

--- personal/aria-css.el
+++ personal/aria-css.el
@@ -1,8 +1,7 @@
 (prelude-require-package 'helm-css-scss)
 
 (defun scss-mode-hook-setting()
-  (setq css-indent-offset 4)
-  (local-set-key (kbd "C-c S") 'helm-css-scss))
+  (setq css-indent-offset 4))
 
 (add-hook 'scss-mode-hook 'scss-mode-hook-setting)
 
